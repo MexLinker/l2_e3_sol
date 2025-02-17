336adc8252b78c6cef34963696cce41bcad991ec
MexLinker <2644087688@qq.com>
2025-02-17 21:44:50 +0800

    added a ADC to the proj

--- src/main.c
+++ src/main.c
@@ -15,6 +15,30 @@
 
 #include <dk_buttons_and_leds.h>
 
+
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/adc.h>
+
+#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
+	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
+#error "No suitable devicetree overlay specified"
+#endif
+
+#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
+	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
+
+/* Data of ADC io-channels specified in devicetree. */
+static const struct adc_dt_spec adc_channels[] = {
+	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
+			     DT_SPEC_AND_COMMA)
+};
+
+
 static const struct bt_le_adv_param *adv_param = BT_LE_ADV_PARAM(
 	(BT_LE_ADV_OPT_CONNECTABLE |
 	 BT_LE_ADV_OPT_USE_IDENTITY), /* Connectable advertising and use identity address */
@@ -72,7 +96,49 @@ int main(void)
 	err = bt_id_create(&addr, NULL);
 	err = bt_enable(NULL);
 
+
+
+	// adc configs
+
+	uint32_t count = 0;
+	uint16_t buf;
+
+	struct adc_sequence sequence = {
+		.buffer = &buf,
+		/* buffer size in bytes, not number of samples */
+		.buffer_size = sizeof(buf),
+	};
+
+	/* Configure channels individually prior to sampling. */
+	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+		if (!adc_is_ready_dt(&adc_channels[i])) {
+			printk("ADC controller device %s not ready\n", adc_channels[i].dev->name);
+			return 0;
+		}
+
+		err = adc_channel_setup_dt(&adc_channels[i]);
+		if (err < 0) {
+
+			printk("Setting up ADC device: %s, channel: %d\n",
+				adc_channels[i].dev->name, adc_channels[i].channel_id);
+
+			printk("ADC config - gain: %d, reference: %d, acquisition time: %d\n",
+					adc_channels[i].channel_cfg.gain,
+					adc_channels[i].channel_cfg.reference,
+					adc_channels[i].channel_cfg.acquisition_time);
+			 
+
+			printk("Could not setup channel #%d (%d)\n", i, err);
+
+			return 0;
+		}
+	}
+
+	// main do-while
+
 	do {
+
+		// ble adv
 
 
 		k_sleep(K_MSEC(400));
@@ -86,7 +152,7 @@ int main(void)
 
 		k_sleep(K_MSEC(400));
 
-
+		// ble scan
 
 		k_sleep(K_MSEC(400));
 
@@ -98,6 +164,47 @@ int main(void)
 		err = bt_le_scan_stop();
 
 		k_sleep(K_MSEC(400));
+
+		// adc
+
+		printk("ADC reading[%u]:\n", count++);
+		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
+			int32_t val_mv;
+
+			printk("- %s, channel %d: ",
+			       adc_channels[i].dev->name,
+			       adc_channels[i].channel_id);
+
+			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
+
+			err = adc_read_dt(&adc_channels[i], &sequence);
+			if (err < 0) {
+				printk("Could not read (%d)\n", err);
+				continue;
+			}
+
+			/*
+			 * If using differential mode, the 16 bit value
+			 * in the ADC sample buffer should be a signed 2's
+			 * complement value.
+			 */
+			if (adc_channels[i].channel_cfg.differential) {
+				val_mv = (int32_t)((int16_t)buf);
+			} else {
+				val_mv = (int32_t)buf;
+			}
+			printk("%"PRId32, val_mv);
+			err = adc_raw_to_millivolts_dt(&adc_channels[i],
+						       &val_mv);
+			/* conversion to mV may not be supported, skip if not */
+			if (err < 0) {
+				printk(" (value in mV not available)\n");
+			} else {
+				printk(" = %"PRId32" mV\n", val_mv);
+			}
+		}
+
+		k_sleep(K_MSEC(1000));
 
 	} while (1);
 	return 0;
