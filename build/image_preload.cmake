2a513725bb4b815aae298f4baa2f969b3e321139
MexLinker <2644087688@qq.com>
2025-02-17 16:44:43 +0800

    real MacAddr modify

---
+++ build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52dk/nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/users/mexlinkinshanghai/desktop/workinfeb/nrfreal/bt-fund-main/bt-fund-main/v2.9.0-v2.7.0/l2/l2_e3_sol" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
