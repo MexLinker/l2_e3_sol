4ea25c1151f0d7a309dd3cb18f9d97265f9bf783
MexLinker <2644087688@qq.com>
2025-02-17 20:17:09 +0800

    Remove build directory from version control

--- build_1/image_preload.cmake
+++
@@ -1,7 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52dk/nrf52832" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "c:/users/mexlinkinshanghai/desktop/workinfeb_pc/nrf_dev_find_your_heart/l2_e3_sol" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
